950dc68f3da6ec3584638e7c7864c59e4f7638dc
Arun Ashok <arunashok22041996@gmail.com>
2020-07-19 22:25:42 +0530

    Infinite Scrolldown

--- src/College.js
+++ src/College.js
@@ -1,109 +1,51 @@
 import React, { Component }  from 'react';
 import "./College.css";
 import colleges from './colleges.json';
-import InfiniteScroll from "react-infinite-scroll-component"
+import InfiniteScroll from "react-infinite-scroll-component";
+import { render } from "react-dom";
 
 class College extends React.Component
 {
-  
+  i=10;
+  state = {
+    colleges: colleges.colleges.slice(0,10),
+    hasMore: true
+  };
 
-  constructor(){
-    super();
-    this.state={colleges:[]}
-  }
+  fetchMoreData = () => {
+    if(this.i==colleges.colleges.length){
+      this.setState({hasMore:false});
+      return;
+    }
+    this.setState({colleges:this.state.colleges.concat(colleges.colleges.slice(this.i,this.i+10))});
+    this.i=this.i+10;
+    console.log(this.state);
+
+  };
+
 
 render() {
-  this.state.colleges=Object.values(colleges.colleges);
-  var data=this.state.colleges;
-  var data1=JSON.stringify(data[0])
     return (  
-<div>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <h3>Card 1</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-  <div class="column">
-    <div class="card">
-      <h3>Card 2</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
- </div>
- <div class="row">
-  <div class="column">
-    <div class="card">
-      <h3>Card 3</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-  <div class="column">
-    <div class="card">
-      <h3>Card 4</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-</div>
- <div class="row">
-  <div class="column">
-    <div class="card">
-      <h3>Card 5</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-  
-  <div class="column">
-    <div class="card">
-      <h3>Card 6</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-</div>
- <div class="row">
-  <div class="column">
-    <div class="card">
-      <h3>Card 7</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-  
-  <div class="column">
-    <div class="card">
-      <h3>Card 8</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-</div>
- <div class="row">
-  <div class="column">
-    <div class="card">
-      <h3>Card 9</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-  
-  <div class="column">
-    <div class="card">
-      <h3>Card 10</h3>
-      <p>Some text</p>
-      <p>Some text</p>
-    </div>
-  </div>
-</div>
-</div>
-);}
+        <div>
+          <InfiniteScroll
+            dataLength={this.state.colleges.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.hasMore}
+            loader={<h4>Loading...</h4>}>
+            {this.state.colleges.map((i, index) => (
+              <div key={index}>
+                <div class="column">
+                  <div class="card">
+                    <h3>{this.state.colleges[index]['college_name']}</h3>
+                    <p>Some text</p>
+                    <p>Some text</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </InfiniteScroll>
+        </div>
+    );}
 }
-
 
 export default College;
